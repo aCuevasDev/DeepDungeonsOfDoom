c3afbf26ca5bf92a779fece02e6a2fda66325bf0
Alex Cuevas <alu2018311@a22o05.local>
2018-12-19 20:12:34 +0100

    updated view select hero

--- Deep Dungeons of Doom/Model/Heroes/Hero.swift
+++ Deep Dungeons of Doom/Model/Heroes/Hero.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum MyError: Error {
     case runtimeError(String)
--- Deep Dungeons of Doom/Model/View/SelectHeroView.swift
+++ Deep Dungeons of Doom/Model/View/SelectHeroView.swift
@@ -10,40 +10,71 @@ import Foundation
 import UIKit
 
 class SelectHeroView : UIView{
-    var heroImage : UIImage = UIImage()
+    
     var heartImage : UIImage = UIImage()
     var atackImage : UIImage = UIImage()
     var defenseImage : UIImage = UIImage()
     var luckImage : UIImage = UIImage()
     var magicImage : UIImage = UIImage()
+ 
+    //TODO VIEW LOOK GIT DANI
+    //https://github.com/dmorenoar/ViewProgramactlly/blob/master/ViewProgramactlly/VistaPersonalizada.swift
+   //TODO SEPARATE ALL THE VIEWS INTO DIFFERENT METHODS
     
-    var health : Int = 0
-    var attack : Double = 0
-    var defense : Double = 0
-    var luck : Double = 0
-    var magic : Double = 0
-    
-    var name : String = ""
-    
-    var view : UIView = UIView()
-    
-    
-   /* constructor to customize the images
+    /* constructor to customize the images
      init(heroImage : UIImage, heartImage : UIImage, attackImage : UIImage, defenseImage : UIImage,luckImage : UIImage,magicImage : UIImage, hero : Hero) {
         
     } */
     
-    init(hero : Hero){
-        self.health = hero.health
-        self.attack = hero.atk
-        self.defense = hero.def
-        self.luck = hero.lck
-        self.magic = hero.mag
-        self.name = hero.name
-    }
+//    init(hero1,hero2,hero3)
     
-    func getView(){
-        view.
+    func getHeroView(hero : Hero) -> UIView{
+        var heroImage : UIImage = hero.image
+        
+        var name : String = hero.name
+        var health : Int = hero.maxHealth
+        var attack : Double = hero.atk
+        var defense : Double = hero.def
+        var luck : Double = hero.lck
+        var magic : Double = hero.mag
+        
+        let view : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 200))
+        let backgroundView : UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        backgroundView.image = UIImage(named: "back_heroe")
+        view.addSubview(backgroundView)
+       // backgroundView.contentMode = .scaleAspectFit
+        
+        let heroView : UIImageView = UIImageView(image: hero.image)
+        heroView.contentMode = .scaleAspectFit
+        heroView.frame = CGRect(x: 20, y: 20, width: 50, height: 100)
+        view.addSubview(heroView)
+        
+        let nameLabel:UILabel = {
+            let lbl:UILabel = UILabel(frame: CGRect(x: 100, y: 20, width: 60, height: 25))
+            lbl.text = hero.name
+            
+            lbl.textColor = UIColor.white
+            return lbl
+        }()
+        view.addSubview(nameLabel)
+        
+        let atkLabel:UILabel = {
+            let lbl:UILabel = UILabel(frame: CGRect(x: 100, y: 80, width: 40, height: 25))
+            lbl.text = String(hero.atk)
+            lbl.textColor = UIColor.white
+            return lbl
+        }()
+        view.addSubview(atkLabel)
+        
+        let defLabel:UILabel = {
+            let lbl:UILabel = UILabel(frame: CGRect(x: 100, y: 140, width: 40, height: 25))
+            lbl.text = String(hero.atk)
+            lbl.textColor = UIColor.white
+            return lbl
+        }()
+        view.addSubview(defLabel)
+        
+        return view
     }
     
     
