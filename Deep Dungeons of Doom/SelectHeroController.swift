ae00996a6c77f7c06994c3753a5122816737dd22
alex <alex@alexs-Mac.local>
2019-01-07 19:29:06 -0800

    .

--- Deep Dungeons of Doom/SelectHeroController.swift
+++ Deep Dungeons of Doom/SelectHeroController.swift
@@ -12,8 +12,8 @@ import UIKit
 class SelectHeroController : UIViewController {
     
     override func viewDidLoad() {
-        let warrior : Warrior = Warrior()
-        var view : UIView = SelectHeroView(frame: CGRect(x: 15, y: 250, width: 500, height: 500))
+        super.viewDidLoad()
+        let view : UIView = SelectHeroView(frame: CGRect(x: 15, y: 250, width: 350, height: 500))
         self.view.addSubview(view)
     }
 }
