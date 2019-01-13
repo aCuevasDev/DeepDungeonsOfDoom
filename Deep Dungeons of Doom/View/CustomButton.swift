7f6232836f34fafa7756c6b226c389848a141db2
Alex Cuevas <alexcuevitas@gmail.com>
2019-01-13 21:06:08 +0000

    CustomButton.swift edited online with Bitbucket

--- Deep Dungeons of Doom/View/CustomButton.swift
+++ Deep Dungeons of Doom/View/CustomButton.swift
@@ -1,10 +1,3 @@
-//
-//  CustomButton.swift
-//  Deep Dungeons of Doom
-//
-//  Created by alex on 07/01/2019.
-//  Copyright © 2019 Alex. All rights reserved.
-//
 
 import Foundation
 import UIKit
