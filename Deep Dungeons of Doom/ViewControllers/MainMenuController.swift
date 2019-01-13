70b760e3b3bbd48c02a52f021fc3b82aba472a4d
Alex Cuevas <alexcuevitas@gmail.com>
2019-01-13 21:04:13 +0000

    MainMenuController.swift edited online with Bitbucket

--- Deep Dungeons of Doom/ViewControllers/MainMenuController.swift
+++ Deep Dungeons of Doom/ViewControllers/MainMenuController.swift
@@ -1,10 +1,4 @@
-//
-//  MainMenuController.swift
-//  Deep Dungeons of Doom
-//
-//  Created by alex on 07/01/2019.
-//  Copyright © 2019 Alex. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
